4653a6d3d254cfcdecd81c30fbcdf1d1da21ebfe
Nish <dmpy.it@gmail.com>
2019-09-11 11:56:29 -0300

    Prototype Design Pattern Files

---
+++ Prototype/Movie.java
@@ -0,0 +1,16 @@
+package com.dp.Prototype;
+
+public class Movie extends Item {
+	
+	private String runtime;
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	
+
+}
